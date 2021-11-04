fa0ee9ed6844e28ac689e65f8dc8dbec71395698
Daron Spence <4062087+Daronspence@users.noreply.github.com>
2021-11-04 16:27:20 -0500

    Audio and video block support

---
+++ inc/blocks/core/class-video.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * WP Component Library includes: Video Block class
+ *
+ * @package WP_Component_Library
+ */
+
+namespace WP_Component_Library\Blocks\Core;
+
+use WP_Component_Library\Blocks\Anonymous;
+
+/**
+ * Renders a Video block as a WPCL component.
+ */
+class Video extends Anonymous {
+
+	/**
+	 * Render the the block.
+	 *
+	 * @return void
+	 */
+	public function render(): void {
+		$root  = $this->dom_parser->firstChild();
+		$video = $root->find( 'video', 0 );
+
+		foreach ( $root->getAttributes() as $attr => $value ) {
+			$this->attrs[ $attr ] = $value;
+		}
+
+		// Attributes from the Video element for loop/controls/autoplay/etc.
+		foreach ( optional( $video )->getAttributes() ?? [] as $attr => $value ) {
+			$this->attrs[ $attr ] = $value ?? true;
+		}
+
+		$this->render_component( $this->attrs );
+	}
+}
