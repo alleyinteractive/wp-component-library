8c62e67de8da6406e7038858b9f4596c939bb741
Daron Spence <4062087+Daronspence@users.noreply.github.com>
2021-11-12 11:03:26 -0600

    PHPCS

--- components/wpcl-iframe-preview/template.php
+++ components/wpcl-iframe-preview/template.php
@@ -11,7 +11,7 @@
 	<?php wpcl_attributes( [ 'src' => $args['src'] ], $args ); ?>
 	style="
 		width: <?php echo esc_attr( $args['width'] ); ?>;
-		height: <?php echo esc_attr($args['height']); ?>;
+		height: <?php echo esc_attr( $args['height'] ); ?>;
 		background: rgba(0,0,0,0.25);
 	"
 ></iframe>
