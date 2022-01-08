7e0edc604b1579c2ed6a962f25ed7de83833389e
jomurgel <dev@jomurgel.com>
2022-01-08 15:22:11 -0700

    improves components sidebar/iframe view

--- components/wpcl-layouts/sidebar-content/template.php
+++ components/wpcl-layouts/sidebar-content/template.php
@@ -7,8 +7,8 @@
  */
 
 ?>
-<div style="display: grid; grid-gap: 1rem; grid-template-columns: minmax(min-content, 300px) 1fr;">
-	<div style="max-width: 300px;">
+<div style="display: grid; grid-gap: 1rem; grid-template-columns: minmax(min-content, 300px) 1fr; max-width: calc(100% - 15px);">
+	<div style="max-width: 300px; max-height: 85vh; overflow-y: scroll;">
 		<?php echo $args['sidebar']; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 	</div>
 	<div>
