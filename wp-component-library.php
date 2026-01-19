c6aeb359a293d8424c4b033525790bf02ec65c84
Sean Fisher <srtfisher@gmail.com>
2026-01-19 11:17:50 -0500

    CHANGELOG

--- wp-component-library.php
+++ wp-component-library.php
@@ -11,7 +11,7 @@
  * Plugin Name: WP Component Library
  * Plugin URI: https://github.com/alleyinteractive/wp-component-library
  * Description: Component library functionality for your WordPress theme.
- * Version: 1.0.1
+ * Version: 1.1.0
  * Author: Alley
  */
 
